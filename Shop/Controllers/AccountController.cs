70eb240785f02045d99a24b2195be6d5be36230f
Cekopi4a <cecko207@abv.bg>
2024-01-30 23:57:29 +0200

    1

---
+++ Shop/Controllers/AccountController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Shop.Models;
+using Shop.DBModel;
+
+namespace Shop.Controllers
+{
+    public class AccountController : Controller
+    {
+        ShopEntities objUserDBEntities1 = new ShopEntities();
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            UserModel objUserModel = new UserModel();
+            return View(objUserModel);
+        }
+
+        [HttpPost]
+        public ActionResult Register(UserModel objUserModel)
+        {
+            if (ModelState.IsValid)
+            {
+                if (!objUserDbEntities.Users.Any(m => m.Email == objUserModel.Email))
+                {
+                    User objUser = new DBModel.User();
+                    objUser.CreatedOn = DateTime.Now;
+                    objUser.Email = objUserModel.Email;
+                    objUser.FirstName = objUserModel.FirstName;
+                    objUser.LastName = objUserModel.LastName;
+                    objUser.Password = objUserModel.Password;
+                    objUser.Role = "Admin";
+                    objUserDbEntities.Users.Add(objUser);
+                    objUserDbEntities.SaveChanges();
+                    objUserModel = new UserModel();
+                    objUserModel.SuccessMessage = "User is Successfully Added!";
+                    return RedirectToAction("Index", "Home");
+                }
+
+                else
+                {
+                    ModelState.AddModelError("Error", "Email is Alredy exists!");
+                    return View();
+                }
+
+            }
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            LoginModel objLoginModel = new LoginModel();
+            return View(objLoginModel);
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel objLoginModel)
+        {
+            if (ModelState.IsValid)
+            {
+                if (objUserDbEntities.Users.Where(m => m.Email == objLoginModel.Email && m.Password == objLoginModel.Password).FirstOrDefault() == null)
+                {
+                    ModelState.AddModelError("Error", "Wrong Email or Password.");
+                    return View();
+                }
+                else
+                {
+                    Session["Email"] = objLoginModel.Email;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Shop/Models/UserModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Shop.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage ="First Name is Required.")]
+        [Display(Name = "First Name: ")]
+        public string FirstName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Last Name is Required.")]
+        [Display(Name = "Last Name: ")]
+        public string LastName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email is Required.")]
+        [EmailAddress(ErrorMessage ="Enter the proper Email.")]
+        [Display(Name = "Email: ")]
+        public string Email { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password is Required.")]
+        [Display(Name = "Password: ")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Confirm Password is Required.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password: ")]
+        [Compare("Password",ErrorMessage ="Password and Confirm Password should be same.")]
+        public string ConfirmPassword { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string SuccessMessage { get; set; }
+    }
+}
